29f43a2933221d7b8676b1bd10c38d766c62eb19
Monique Van Den Berg <moniquevdb109@gmail.com>
2025-11-15 13:07:19 +0200

    created storage functions

--- scripts/utils/localStorage.js
+++ scripts/utils/localStorage.js
@@ -1,8 +1,25 @@
 // Visualize in the browser DevTools → Application → Local Storage
 
+import { initialTasks } from "../../initialData.js"
 
-// Load task data from local storage.
-loadTasksFromStorage() 
+// Load task data from local storage or initialize with default tasks.
+
+export function loadTasksFromStorage() {
+  const stored = localStorage.getItem("tasks");
+  if (stored) {
+    try {
+      return JSON.parse(stored);
+    } catch (err) {
+      console.error("Error parsing tasks from localStorage:", err);
+    } 
+  }
+
+    localStorage.setItem("tasks", JSON.stringify(initialTasks));
+return initialTasks;
+}
 
 // Save task data to local storage.
-saveTasksToStorage()
+
+export function saveTasksToStorage(tasks) {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    }   
