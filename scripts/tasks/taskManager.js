f981c2c2eb7e0955428e434fbe02a63d46a525fb
Monique Van Den Berg <moniquevdb109@gmail.com>
2025-11-15 15:23:47 +0200

    added new task modal in html and css

--- scripts/tasks/taskManager.js
+++ scripts/tasks/taskManager.js
@@ -1,10 +1,3 @@
-// Logic for adding and resetting form
-
-// addNewTask()
-// Read input values
-// Create new task object with unique ID
-// Update localStorage (saveTasksToStorage())
-// Re-render board (renderTasks())
-// Reset form and close modal
+// import render tasks?
 
 
